fee8bb33adeca19884f0cb07dbe4267c3c555490
Jason Limas <jasonandrea14@gmail.com>
2022-04-14 06:13:54 +1000

    Add animated text in intro with ityped.js

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,13 +1,26 @@
 import './intro.scss'
+import { useEffect, useRef } from 'react'
+import { init } from 'ityped'
 
 export default function Intro() {
+  const ref = useRef();
+
+  useEffect(() => {
+      init(ref.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ['Developer', 'Coffee Enthusiast'],
+      })
+  })
+
   return (
     <div className='intro'>
         <div className="left">
             <div className="wrapper">
                 <h2>Hello! My name is</h2>
                 <h1>Jason Limas</h1>
-                <h3>I'm a <span>AUTO TEXT</span></h3>
+                <h3>I'm a <span ref={ref}></span></h3>
             </div>
         </div>
         <div className="right">
