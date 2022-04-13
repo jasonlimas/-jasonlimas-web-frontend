1ae419e869628b06002f2b7c4c9c59c7eb634f72
Jason Limas <jasonandrea14@gmail.com>
2022-04-14 03:51:16 +1000

    Add top menu bar component and its scss file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Topbar from './components/topbar/Topbar'
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import './topbar.scss'
+
+export default function Topbar() {
+  return (
+    <div className='topbar'>
+      Top bar
+      <div className="wrapper">
+        <div className="center">
+          <a href=""></a>
+        </div>
+        <div className="right">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
