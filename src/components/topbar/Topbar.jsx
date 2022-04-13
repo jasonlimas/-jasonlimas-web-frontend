da413f8662a68eb0a79dcb2fbb7c68af315093f3
Jason Limas <jasonandrea14@gmail.com>
2022-04-14 04:01:26 +1000

    Add logo on top menu bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,10 +3,9 @@ import './topbar.scss'
 export default function Topbar() {
   return (
     <div className='topbar'>
-      Top bar
       <div className="wrapper">
         <div className="center">
-          <a href=""></a>
+          <a href="" className="logo">Jason Limas</a>
         </div>
         <div className="right">
           
