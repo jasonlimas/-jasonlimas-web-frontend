c9483ab0c3a8faca4085f4898f0fece0a5368a8c
Jason Limas <jasonandrea14@gmail.com>
2022-04-14 23:50:59 +1000

    Logo and little text in side bar menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -7,11 +7,13 @@ export default function Menu({ menuOpen, setMenuOpen }) {
 
   return (
     <div className={'menu ' + (menuOpen && 'active')}>
+      <img src="assets/logo-white.png" alt="" />
         <ul>
           <ListElement name='INTRO' link='#intro' handleClick={handleClick} />
           <ListElement name='ABOUT' link='#about' handleClick={handleClick} />
           <ListElement name='CONTACT' link='#contact' handleClick={handleClick} />
         </ul> 
+        <p>Built with ❤️ with React.js</p>
     </div>
   )
 }
