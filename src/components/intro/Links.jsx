f98c084bdaf70e0778b2502dac452398b6b74af4
Jason Limas <jasonandrea14@gmail.com>
2022-04-15 04:43:38 +1000

    Responsive contact me section for smaller screens

--- src/components/intro/Links.jsx
+++ src/components/intro/Links.jsx
@@ -4,16 +4,16 @@ import { Instagram, Facebook, LinkedIn, GitHub } from '@material-ui/icons'
 export default function Links() {
   return (
     <div className='links'>
-        <a href='https://www.instagram.com/jasonandrea14/' target='_blank'>
+        <a href='https://www.instagram.com/jasonandrea14/' target='_blank' rel='noreferrer'>
             <Instagram className='icon' />
         </a>
-        <a href="https://www.facebook.com/jasonlimas14/" target='_blank'>
+        <a href="https://www.facebook.com/jasonlimas14/" target='_blank' rel='noreferrer'>
             <Facebook className='icon' />
         </a>
-        <a href="https://www.linkedin.com/in/jason-andrea-limas/" target='_blank'>
+        <a href="https://www.linkedin.com/in/jason-andrea-limas/" target='_blank' rel='noreferrer'>
             <LinkedIn className='icon' />
         </a>
-        <a href="https://github.com/jasonlimas" target='_blank'>
+        <a href="https://github.com/jasonlimas" target='_blank' rel='noreferrer'>
             <GitHub className='icon' />
         </a>
     </div>
